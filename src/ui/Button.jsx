589cf739eb24ce9e315fa96b5b782fe31926b909
hessamsalehi <hessam.salehi1992@gmail.com>
2024-04-16 11:02:19 +0330

    Removed unused comment

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom";
 const commonClasses =
   "inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-slate-600";
 
-// bg-yellow-400
-
 function Button({ children, disabled, to, type = "primary" }) {
   const styles = {
     primary: commonClasses + " px-4 py-3 md:px-6 md:py-4",
